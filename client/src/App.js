5b95d432e63090ffbe9c29dcfda8bc81c5c71b12
Albangh <90769567+Albangh@users.noreply.github.com>
2022-04-27 17:10:49 +0200

    jwt token cookie

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
 
-const App = props => {
+const App = (props) => {
+  const [uid, setUid] = useState(null);
+
+  useEffect(() => {
+    async function fetchToken() {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => setUid(res.data))
+        .catch((err) => console.log("No token"));
+    }
+
+    fetchToken();
+  }, []);
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Routes />
-    </div>
-  )
-}
+    </UidContext.Provider>
+  );
+};
 
-
-export default App
+export default App;
--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import Log from '../components/Log';
+import React, { useContext } from "react";
+import Log from "../components/Log";
+import { UidContext } from "../components/AppContext";
 
-const Profil = props => {
+const Profil = (props) => {
+  const uid = useContext(UidContext);
   return (
-    <div className='profil-page'>
-      <div className='log-container'>
-        <Log signin={false} signup={true} />
-        <div className='img-container'>
-          <img src='./img/log.svg' alt="img-login" />
+    <div className="profil-page">
+      {uid ? (
+        <h1>Update page</h1>
+      ) : (
+        <div className="log-container">
+          <Log signin={false} signup={true} />
+          <div className="img-container">
+            <img src="./img/log.svg" alt="img-login" />
+          </div>
         </div>
-      </div>
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default Profil;
