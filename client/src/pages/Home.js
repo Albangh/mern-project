887e1097be6c3466beeab9852f492362c3479ed9
Albangh <90769567+Albangh@users.noreply.github.com>
2022-04-27 15:56:45 +0200

    config front

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Home = props => {
+  return (
+    <div>Hello Home</div>
+  )
+}
+
+
+export default Home;
---
+++ client/src/pages/Profil.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Profil = props => {
+  return (
+    <div>Profil</div>
+  )
+}
+
+
+export default Profil;
