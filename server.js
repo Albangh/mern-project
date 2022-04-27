31cf4ec17265b67c2f492067c130cd4a6003f472
Albangh <90769567+Albangh@users.noreply.github.com>
2022-04-27 14:40:11 +0200

    crud post

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes.js');
+const postRoutes = require('./routes/post.routes.js');
 require("dotenv").config({path: './config/.env'});
 require("./config/db");
 const {checkUser, requireAuth} = require('./middlewares/auth.middleware');
@@ -18,7 +19,8 @@ app.get('/jwtid', requireAuth, (req,res) => {
 })
 
 //routes
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 app.listen(process.env.PORT, () => {
    console.log("Listen on port ", process.env.PORT);
