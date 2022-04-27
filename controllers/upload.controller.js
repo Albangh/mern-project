932300039b3464a047bff937884fa16d077c0a7f
Albangh <90769567+Albangh@users.noreply.github.com>
2022-04-27 15:23:19 +0200

    multer upload files

---
+++ controllers/upload.controller.js
@@ -0,0 +1,42 @@
+const UserModel = require("../models/user.model");
+const fs = require("fs");
+const { promisify } = require("util");
+const { uploadErrors } = require("../utils/errors.utils");
+const pipeline = promisify(require("stream").pipeline);
+
+module.exports.updloadProfil = async (req, res) => {
+  try {
+    if (
+      req.file.detectedMimeType !== "image/jpg" &&
+      req.file.detectedMimeType !== "image.png" &&
+      req.file.detectedMimeType !== "image/jepg"
+    )
+      throw Error("invalid file");
+
+    if (req.file.size > 500000) throw Error("max size");
+  } catch (error) {
+    const errors = uploadErrors(err);
+    return res.status(201).json({ errors });
+  }
+
+  const fileName = req.body.name + ".jpg";
+
+  await pipeline(
+    req.file.stream,
+    fs.createWriteStream(
+      `${__dirname}/../client/public/uploads/profil/${fileName}`
+    )
+  );
+
+  try {
+    await UserModel.findByIdAndUpdate(
+      req.body.userId,
+      { $set: { picture: "./uploads/profil/" + fileName } },
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+    )
+      .then((docs) => res.send(docs))
+      .catch((err) => res.status(500).send({ message: err }));
+  } catch (error) {
+     return res.status(500).send({ message: err })
+  }
+};
