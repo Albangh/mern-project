2731c7d9bd58a621c92551dbe32b77a1b2455eca
Albangh <90769567+Albangh@users.noreply.github.com>
2022-04-28 12:52:49 +0200

    navbar + logout

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,9 +3,11 @@ import React from "react";
 import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Trending from "../../pages/Trending";
+import Navbar from "../Navbar";
 
 const index = () => {
   return <Router>
+     <Navbar />
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profil" element={<Profil />} />
