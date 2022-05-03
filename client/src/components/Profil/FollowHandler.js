c5e949f63df26391ef3035033c896707bceaa20e
Albangh <90769567+Albangh@users.noreply.github.com>
2022-05-03 15:03:07 +0200

    homepage + like/unlike

--- client/src/components/Profil/FollowHandler.js
+++ client/src/components/Profil/FollowHandler.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { followUser, unfollowUser } from "../../actions/user.actions";
 import { isEmpty } from "../Utils";
 
-const FollowHandler = ({ idToFollow }) => {
+const FollowHandler = ({ idToFollow, type }) => {
   const userData = useSelector((state) => state.userReducer);
   const [isFollowed, setIsFollowed] = useState(false);
   const dispatch = useDispatch();
@@ -15,7 +15,7 @@ const FollowHandler = ({ idToFollow }) => {
 
   const handleUnfollow = () => {
     dispatch(unfollowUser(userData._id, idToFollow));
-    setIsFollowed(false)
+    setIsFollowed(false);
   };
 
   useEffect(() => {
@@ -30,12 +30,22 @@ const FollowHandler = ({ idToFollow }) => {
     <>
       {isFollowed && !isEmpty(userData) && (
         <span onClick={handleUnfollow}>
-          <button className="unfollow-btn">Abonne</button>
+          {type === "suggestion" && (
+            <button className="unfollow-btn">Abonne</button>
+          )}
+          {type === "card" && (
+            <img src="./img/icons/checked.svg" alt="checked" />
+          )}
         </span>
       )}
       {isFollowed === false && !isEmpty(userData) && (
         <span onClick={handleFollow}>
-          <button className="follow-btn">Suivre</button>
+          {type === "suggestion" && (
+            <button className="follow-btn">Suivre</button>
+          )}
+          {type === "card" && (
+            <img src="./img/icons/check.svg" alt="checked" />
+          )}
         </span>
       )}
     </>
